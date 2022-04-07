337b41f55a735e9ecf0399ad7c5c2ffbb86e26e9
ehsaasgrover <ehsaas.grover@hotmail.com>
2022-04-07 15:32:26 +1200

    [002] Hello World

--- src/main/java/com/example/springDemo2021/SpringDemo2021Application.java
+++ src/main/java/com/example/springDemo2021/SpringDemo2021Application.java
@@ -2,12 +2,20 @@ package com.example.springDemo2021;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringDemo2021Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemo2021Application.class, args);
+	}
+
+	@GetMapping
+	public String hello() {
+		return "Hello World";
 	}
 
 }
