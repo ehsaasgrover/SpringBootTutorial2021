ea2d88c4b632f00cd20170439bfa47e592ff8fc7
ehsaasgrover <ehsaas.grover@hotmail.com>
2022-04-07 16:54:51 +1200

    [006] Creating postgres database and mapping student table to database

--- src/main/java/com/example/springDemo2021/student/Student.java
+++ src/main/java/com/example/springDemo2021/student/Student.java
@@ -1,8 +1,22 @@
 package com.example.springDemo2021.student;
 
+import javax.annotation.processing.Generated;
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
