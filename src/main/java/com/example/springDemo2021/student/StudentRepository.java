1f764930328cc5d7e90e46f6fea252923877996a
ehsaasgrover <ehsaas.grover@hotmail.com>
2022-04-08 11:32:31 +1200

    [008] Being able to POST to database and get error message if email is taken, and not updating database

--- src/main/java/com/example/springDemo2021/student/StudentRepository.java
+++ src/main/java/com/example/springDemo2021/student/StudentRepository.java
@@ -1,2 +1,18 @@
-package com.example.springDemo2021.student;public interface StudentRepository {
+package com.example.springDemo2021.student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface StudentRepository
+        extends JpaRepository<Student, Long> {
+
+    // SELECT * FROM student WHERE email = ?
+    // this student is the class not the database
+    @Query("SELECT s FROM Student s WHERE s.email =?1")
+    Optional<Student> findStudentByEmail(String email);
+
 }
--- src/main/java/com/example/springDemo2021/student/StudentService.java
+++ src/main/java/com/example/springDemo2021/student/StudentService.java
@@ -1,24 +1,32 @@
 package com.example.springDemo2021.student;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Mariam",
-                        "mariam.jamal@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY, 5),
-                        21
-                )
-        );
+        return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if (studentOptional.isPresent()) {
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
     }
 }
